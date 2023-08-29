a1da10e5b24ac9fede8871f4fa62e0e248cc8dab
AZEVEDO DE ARAGAO NOGUEIRA Antonio Jose 00057875 <antonio.nogueira@3ds.com>
2023-08-29 12:41:45 +0200

    risks 2023X FD03 updates

--- ws3dx.project/risk/data/IResponseRisk.cs
+++ ws3dx.project/risk/data/IResponseRisk.cs
@@ -19,9 +19,9 @@ namespace ws3dx.project.risk.data
 {
    public interface IResponseRisk
    {
-      public bool Success { get; set; }
+      public bool? Success { get; set; }
 
-      public int StatusCode { get; set; }
+      public int? StatusCode { get; set; }
 
       public ICsrf Csrf { get; set; }
 
