5be986e1d4671089be07de4a068f85f85b252b16
AZEVEDO DE ARAGAO NOGUEIRA Antonio Jose 00057875 <antonio.nogueira@3ds.com>
2023-10-12 14:23:53 +0200

    #24 review project.task 2023XFD04 (fully compatible) - tests refactoring and Readme

--- ws3dx.project/task/tests/service.tests/TaskService_Task_UnitTest.cs
+++ ws3dx.project/task/tests/service.tests/TaskService_Task_UnitTest.cs
@@ -14,14 +14,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //------------------------------------------------------------------------------------------------------------------------------------
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using ws3dx.authentication.data;
-using ws3dx.authentication.data.impl.passport;
-using ws3dx.core.data.impl;
 using ws3dx.core.exception;
-using ws3dx.core.redirection;
 using ws3dx.project.task.core.data.impl;
 using ws3dx.project.task.core.service;
 using ws3dx.project.task.data;
@@ -30,78 +24,14 @@ using WS3DX = ws3dx.project.task.core.data.impl;
 
 namespace NUnitTestProject
 {
-   public class TaskServiceTests
+   public class TaskService_Task_UnitTests : TaskServiceTestsSetup
    {
-      const string DS3DXWS_AUTH_USERNAME = "DS3DXWS_AUTH_USERNAME";
-      const string DS3DXWS_AUTH_PASSWORD = "DS3DXWS_AUTH_PASSWORD";
-      const string DS3DXWS_AUTH_PASSPORT = "DS3DXWS_AUTH_PASSPORT";
-      const string DS3DXWS_AUTH_ENOVIA = "DS3DXWS_AUTH_ENOVIA";
-      const string DS3DXWS_AUTH_TENANT = "DS3DXWS_AUTH_TENANT";
-      const string SECURITY_CONTEXT = "VPLMProjectLeader.Company Name.AAA27 Personal";
-
-      const string CUSTOM_PROP_NAME_1_DBL = "AAA27_REAL_TEST";
-      const string CUSTOM_PROP_NAME_2_INT = "AAA27_INT_TEST";
-
-      string m_username = string.Empty;
-      string m_password = string.Empty;
-      string m_passportUrl = string.Empty;
-      string m_serviceUrl = string.Empty;
-      string m_tenant = string.Empty;
-
-      UserInfo m_userInfo = null;
-
-      public async Task<IPassportAuthentication> Authenticate()
-      {
-         UserPassport passport = new UserPassport(m_passportUrl);
-
-         UserInfoRedirection userInfoRedirection = new UserInfoRedirection(m_serviceUrl, m_tenant)
-         {
-            Current = true,
-            IncludeCollaborativeSpaces = true,
-            IncludePreferredCredentials = true
-         };
-
-         m_userInfo = await passport.CASLoginWithRedirection<UserInfo>(m_username, m_password, false, userInfoRedirection);
-
-         Assert.IsNotNull(m_userInfo);
-
-         StringAssert.AreEqualIgnoringCase(m_userInfo.name, m_username);
-
-         Assert.IsTrue(passport.IsCookieAuthenticated);
-
-         return passport;
-      }
-
-      [SetUp]
-      public void Setup()
-      {
-         m_username = Environment.GetEnvironmentVariable(DS3DXWS_AUTH_USERNAME, EnvironmentVariableTarget.User); // e.g. AAA27
-         m_password = Environment.GetEnvironmentVariable(DS3DXWS_AUTH_PASSWORD, EnvironmentVariableTarget.User); // e.g. your password
-         m_passportUrl = Environment.GetEnvironmentVariable(DS3DXWS_AUTH_PASSPORT, EnvironmentVariableTarget.User); // e.g. https://eu1-ds-iam.3dexperience.3ds.com:443/3DPassport
-
-         m_serviceUrl = Environment.GetEnvironmentVariable(DS3DXWS_AUTH_ENOVIA, EnvironmentVariableTarget.User); // e.g. https://r1132100982379-eu1-space.3dexperience.3ds.com:443/enovia
-         m_tenant = Environment.GetEnvironmentVariable(DS3DXWS_AUTH_TENANT, EnvironmentVariableTarget.User); // e.g. R1132100982379
-      }
-
-      public string GetDefaultSecurityContext()
-      {
-         return m_userInfo.preferredcredentials.ToString();
-      }
-
-      public TaskService ServiceFactoryCreate(IPassportAuthentication _passport, string _serviceUrl, string _tenant)
-      {
-         TaskService __taskService = new TaskService(_serviceUrl, _passport);
-         __taskService.Tenant = _tenant;
-         __taskService.SecurityContext = GetDefaultSecurityContext();
-         return __taskService;
-      }
 
       [TestCase("")]
       public async Task GetTaskReferences(string taskId)
       {
-         IPassportAuthentication passport = await Authenticate();
+         TaskService taskService = ServiceFactoryCreate(await Authenticate());
 
-         TaskService taskService = ServiceFactoryCreate(passport, m_serviceUrl, m_tenant);
          IList<IResponseReferenceData> ret = await taskService.GetTaskReferences(taskId);
 
          Assert.IsNotNull(ret);
@@ -110,9 +40,8 @@ namespace NUnitTestProject
       [TestCase("A0EA6A14EC2400006363763A0004E226")]
       public async Task GetTask(string taskId)
       {
-         IPassportAuthentication passport = await Authenticate();
+         TaskService taskService = ServiceFactoryCreate(await Authenticate());
 
-         TaskService taskService = ServiceFactoryCreate(passport, m_serviceUrl, m_tenant);
          IResponseTaskData ret = await taskService.GetTask(taskId);
 
          Assert.IsNotNull(ret);
@@ -121,9 +50,8 @@ namespace NUnitTestProject
       [TestCase("A0EA6A14EC2400006363763A0004E226")]
       public async Task GetTaskAssignees(string taskId)
       {
-         IPassportAuthentication passport = await Authenticate();
+         TaskService taskService = ServiceFactoryCreate(await Authenticate());
 
-         TaskService taskService = ServiceFactoryCreate(passport, m_serviceUrl, m_tenant);
          IList<IResponseAssigneeData> ret = await taskService.GetTaskAssignees(taskId);
 
          Assert.IsNotNull(ret);
@@ -132,20 +60,18 @@ namespace NUnitTestProject
       [TestCase("795A0F6C3253000061FDA1A70013466B")]
       public async Task GetTaskScopes(string taskId)
       {
-         IPassportAuthentication passport = await Authenticate();
+         TaskService taskService = ServiceFactoryCreate(await Authenticate());
 
-         TaskService taskService = ServiceFactoryCreate(passport, m_serviceUrl, m_tenant);
          IList<IResponseScopeData> ret = await taskService.GetTaskScopes(taskId);
 
          Assert.IsNotNull(ret);
       }
 
-      [TestCase("FDBEB6D4D7020000639849FE0001DE07")]//Change Action
+      [TestCase("FDBEB6D4D7020000639849FE0001DE07")]
       public async Task GetTasksWithScope(string scopeId)
       {
-         IPassportAuthentication passport = await Authenticate();
+         TaskService taskService = ServiceFactoryCreate(await Authenticate());
 
-         TaskService taskService = ServiceFactoryCreate(passport, m_serviceUrl, m_tenant);
          IList<IResponseTaskData> ret = await taskService.GetTasksWithScope(scopeId);
 
          Assert.IsNotNull(ret);
@@ -154,9 +80,8 @@ namespace NUnitTestProject
       [TestCase("A0EA6A14EC2400006363763A0004E226")]
       public async Task GetTaskDeliverables(string taskId)
       {
-         IPassportAuthentication passport = await Authenticate();
+         TaskService taskService = ServiceFactoryCreate(await Authenticate());
 
-         TaskService taskService = ServiceFactoryCreate(passport, m_serviceUrl, m_tenant);
          IList<IResponseDeliverableData> ret = await taskService.GetTaskDeliverables(taskId);
 
          Assert.IsNotNull(ret);
@@ -165,11 +90,9 @@ namespace NUnitTestProject
       [TestCase(true)]
       public async Task GetUserTasks(bool showProjectTasks)
       {
-         IPassportAuthentication passport = await Authenticate();
+         TaskService taskService = ServiceFactoryCreate(await Authenticate());
 
-         TaskService taskService = ServiceFactoryCreate(passport, m_serviceUrl, m_tenant);
          IList<IResponseTaskData> ret = await taskService.GetUserTasks(showProjectTasks);
-         Assert.IsNotNull(ret);
 
          foreach (IResponseTaskData taskData in ret)
          {
@@ -183,12 +106,10 @@ namespace NUnitTestProject
       [TestCase()]
       public async Task GetTasks()
       {
-         IPassportAuthentication passport = await Authenticate();
-
-         TaskService taskService = ServiceFactoryCreate(passport, m_serviceUrl, m_tenant);
+         TaskService taskService = ServiceFactoryCreate(await Authenticate());
 
          // Comma-separated list of IDs to retrieve.
-         string idPayload = "A0EA6A14EC2400006363763A0004E226";
+         const string idPayload = "A0EA6A14EC2400006363763A0004E226";
 
          try
          {
@@ -206,9 +127,7 @@ namespace NUnitTestProject
       [TestCase("")]
       public async Task AddAssigneesToTask(string taskId)
       {
-         IPassportAuthentication passport = await Authenticate();
-
-         TaskService taskService = ServiceFactoryCreate(passport, m_serviceUrl, m_tenant);
+         TaskService taskService = ServiceFactoryCreate(await Authenticate());
 
          IAssignees assignees = new Assignees();
 
@@ -217,6 +136,7 @@ namespace NUnitTestProject
             IList<IResponseAssigneeData> ret = await taskService.AddAssigneesToTask(taskId, assignees);
 
             Assert.IsNotNull(ret);
+
          }
          catch (HttpResponseException _ex)
          {
@@ -228,9 +148,7 @@ namespace NUnitTestProject
       [TestCase("")]
       public async Task AddReferencesToTask(string taskId)
       {
-         IPassportAuthentication passport = await Authenticate();
-
-         TaskService taskService = ServiceFactoryCreate(passport, m_serviceUrl, m_tenant);
+         TaskService taskService = ServiceFactoryCreate(await Authenticate());
 
          IReferences references = new References();
 
@@ -239,6 +157,7 @@ namespace NUnitTestProject
             IList<IResponseReferenceData> ret = await taskService.AddReferencesToTask(taskId, references);
 
             Assert.IsNotNull(ret);
+
          }
          catch (HttpResponseException _ex)
          {
@@ -250,9 +169,7 @@ namespace NUnitTestProject
       [TestCase("")]
       public async Task AddDeliverablesToTask(string taskId)
       {
-         IPassportAuthentication passport = await Authenticate();
-
-         TaskService taskService = ServiceFactoryCreate(passport, m_serviceUrl, m_tenant);
+         TaskService taskService = ServiceFactoryCreate(await Authenticate());
 
          IDeliverables deliverables = new Deliverables();
 
@@ -261,6 +178,7 @@ namespace NUnitTestProject
             IList<IResponseDeliverableData> ret = await taskService.AddDeliverablesToTask(taskId, deliverables);
 
             Assert.IsNotNull(ret);
+
          }
          catch (HttpResponseException _ex)
          {
@@ -272,9 +190,7 @@ namespace NUnitTestProject
       [TestCase("")]
       public async Task AddScopesToTask(string taskId)
       {
-         IPassportAuthentication passport = await Authenticate();
-
-         TaskService taskService = ServiceFactoryCreate(passport, m_serviceUrl, m_tenant);
+         TaskService taskService = ServiceFactoryCreate(await Authenticate());
 
          IScopes scopes = new Scopes();
 
@@ -283,6 +199,7 @@ namespace NUnitTestProject
             IList<IResponseScopeData> ret = await taskService.AddScopesToTask(taskId, scopes);
 
             Assert.IsNotNull(ret);
+
          }
          catch (HttpResponseException _ex)
          {
@@ -291,12 +208,12 @@ namespace NUnitTestProject
          }
       }
 
+
       [TestCase("Task III from ws", "Task IV from ws")]
       public async Task CreateTask(string _task1Title, string _task2Title)
       {
-         IPassportAuthentication passport = await Authenticate();
+         TaskService taskService = ServiceFactoryCreate(await Authenticate());
 
-         TaskService taskService = ServiceFactoryCreate(passport, m_serviceUrl, m_tenant);
 
          ITaskData taskData1 = new WS3DX.TaskData();
          taskData1.Title = _task1Title;
@@ -319,6 +236,26 @@ namespace NUnitTestProject
          {
             IList<IResponseTaskData> ret = await taskService.CreateTask(tasks);
             Assert.IsNotNull(ret);
+         }
+         catch (HttpResponseException _ex)
+         {
+            string errorMessage = await _ex.GetErrorMessage();
+            Assert.Fail(errorMessage);
+         }
+      }
+
+      [TestCase("")]
+      public async Task DeleteTask(string taskId)
+      {
+         TaskService taskService = ServiceFactoryCreate(await Authenticate());
+
+
+         try
+         {
+            IRESULTS_EMPTY ret = await taskService.DeleteTask(taskId);
+
+            Assert.IsNotNull(ret);
+
          }
          catch (HttpResponseException _ex)
          {
