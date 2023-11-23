10dc30e2492cbfe4485967c9d82a3c8d20557674
AZEVEDO DE ARAGAO NOGUEIRA Antonio Jose 00057875 <antonio.nogueira@3ds.com>
2023-11-23 14:40:52 +0100

    #28 Allow generic JsonElement return collection

--- ws3dx.core/serialization/CoreSchemaDeserializer.cs
+++ ws3dx.core/serialization/CoreSchemaDeserializer.cs
@@ -52,7 +52,7 @@ namespace ws3dx.core.serialization
          return _Deserialize(jsonContent, typeof(T));
       }
 
-      public IList<T> DeserializeCollection(string jsonContent, string _wrappingCollectionProperty, bool _ignoreIfPropertyNotFound = false)
+      public virtual IList<T> DeserializeCollection(string jsonContent, string _wrappingCollectionProperty, bool _ignoreIfPropertyNotFound = false)
       {
          using JsonDocument jsonDocument = JsonDocument.Parse(jsonContent);
 
