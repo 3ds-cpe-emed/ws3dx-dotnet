e2ed8a79b721b0023ab5f5df71371ed3f5f0e4a6
Antonio Nogueira <antonio.nogueira@3ds.com>
2024-07-30 15:17:22 +0200

    fix with default $mva

--- ws3dx.core/service/SearchService.cs
+++ ws3dx.core/service/SearchService.cs
@@ -27,104 +27,105 @@ using ws3dx.utils.search;
 
 namespace ws3dx.core.service
 {
-   public abstract class SearchService : EnoviaBaseService
-   {
-      private const long MAX_VALS_PER_QUERY = 1000;
-      private const long TOP = MAX_VALS_PER_QUERY;
+    public abstract class SearchService : EnoviaBaseService
+    {
+        private const long MAX_VALS_PER_QUERY = 1000;
+        private const long TOP = MAX_VALS_PER_QUERY;
 
-      private const string SKIP_PARAM_NAME = "$skip";
-      private const string TOP_PARAM_NAME = "$top";
-      private const string SEARCH_CRITERIA_PARAM_NAME = "$searchStr";
+        private const string SKIP_PARAM_NAME = "$skip";
+        private const string TOP_PARAM_NAME = "$top";
+        private const string SEARCH_CRITERIA_PARAM_NAME = "$searchStr";
 
-      protected virtual string GetSearchSkipParamName() { return SKIP_PARAM_NAME; }
-      protected virtual string GetSearchTopParamName() { return TOP_PARAM_NAME; }
-      protected virtual string GetSearchCriteriaParamName() { return SEARCH_CRITERIA_PARAM_NAME; }
+        protected virtual string GetSearchSkipParamName() { return SKIP_PARAM_NAME; }
+        protected virtual string GetSearchTopParamName() { return TOP_PARAM_NAME; }
+        protected virtual string GetSearchCriteriaParamName() { return SEARCH_CRITERIA_PARAM_NAME; }
 
-      protected abstract string GetSearchResource();
+        protected abstract string GetSearchResource();
 
-      protected abstract IEnumerable<Type> SearchConstraintTypes();
+        protected abstract IEnumerable<Type> SearchConstraintTypes();
 
-      protected SearchService(string _enoviaService, IPassportAuthentication passport) : base(_enoviaService, passport)
-      {
-      }
+        protected SearchService(string _enoviaService, IPassportAuthentication passport) : base(_enoviaService, passport)
+        {
+        }
 
-      #region Search
+        #region Search
 
-      protected bool IsSearchSkipParamNameEmpty { get { return string.IsNullOrEmpty(GetSearchSkipParamName()); } }
-      protected bool IsSearchTopParamNameEmpty { get { return string.IsNullOrEmpty(GetSearchTopParamName()); } }
+        protected bool IsSearchSkipParamNameEmpty { get { return string.IsNullOrEmpty(GetSearchSkipParamName()); } }
+        protected bool IsSearchTopParamNameEmpty { get { return string.IsNullOrEmpty(GetSearchTopParamName()); } }
 
-      protected async Task<IList<T>> SearchCollection<T>(string _wrappingCollectionJsonPropertyName, SearchQuery _searchString)
-      {
-         GenericParameterConstraintUtils.CheckConstraints(typeof(T), SearchConstraintTypes());
+        protected async Task<IList<T>> SearchCollection<T>(string _wrappingCollectionJsonPropertyName, SearchQuery _searchString)
+        {
+            GenericParameterConstraintUtils.CheckConstraints(typeof(T), SearchConstraintTypes());
 
-         long skip = 0;
+            long skip = 0;
 
-         List<T> __output = new List<T>();
+            List<T> __output = new List<T>();
 
-         IList<T> page;
+            IList<T> page;
 
-         // const int WAIT_MILISECS = 250;
-         // bool firstTime = true;
+            // const int WAIT_MILISECS = 250;
+            // bool firstTime = true;
 
-         do
-         {
-            //TODO: Add an wait interval
-            // if (!firstTime)
-            // {
-            //   await Task.Delay(WAIT_MILISECS);
-            // }
-            // firstTime = false;
-
-            page = await SearchCollection<T>(_wrappingCollectionJsonPropertyName, _searchString, skip, TOP);
-
-            skip += TOP;
-
-            if (page?.Count > 0)
+            do
             {
-               __output.AddRange(page);
+                //TODO: Add an wait interval
+                // if (!firstTime)
+                // {
+                //   await Task.Delay(WAIT_MILISECS);
+                // }
+                // firstTime = false;
+
+                page = await SearchCollection<T>(_wrappingCollectionJsonPropertyName, _searchString, skip, TOP);
+
+                skip += TOP;
+
+                if (page?.Count > 0)
+                {
+                    __output.AddRange(page);
+                }
             }
-         }
-         while (page?.Count == TOP);
+            while (page?.Count == TOP);
 
-         return __output;
-      }
+            return __output;
+        }
 
-      protected async Task<IList<T>> SearchCollection<T>(string _wrappingCollectionJsonPropertyName, SearchQuery _searchQuery, long _skip = 0, long _top = 100)
-      {
-         System.Diagnostics.Debug.WriteLine($"SearchCollection<{typeof(T).Name}>");
+        protected async Task<IList<T>> SearchCollection<T>(string _wrappingCollectionJsonPropertyName, SearchQuery _searchQuery, long _skip = 0, long _top = 100)
+        {
+            System.Diagnostics.Debug.WriteLine($"SearchCollection<{typeof(T).Name}>");
 
-         GenericParameterConstraintUtils.CheckConstraints(typeof(T), SearchConstraintTypes());
+            GenericParameterConstraintUtils.CheckConstraints(typeof(T), SearchConstraintTypes());
 
-         Dictionary<string, string> queryParams = new Dictionary<string, string>();
+            Dictionary<string, string> queryParams = new Dictionary<string, string>();
 
-         if (HasMask)
-         {
-            string maskValueFromType = MaskNameUtils.GetMaskNameFromType(typeof(T), false);
-
-            if (maskValueFromType != null)
+            if (HasMask)
             {
-               queryParams.Add(GetMaskParamName(), maskValueFromType);
+                string maskValueFromType = MaskNameUtils.GetMaskNameFromType(typeof(T), false);
+
+                if (maskValueFromType != null)
+                {
+                    queryParams.Add(GetMaskParamName(), maskValueFromType);
+                }
             }
-         }
 
-         if (!IsSearchSkipParamNameEmpty) queryParams.Add(GetSearchSkipParamName(), _skip.ToString());
-         if (!IsSearchTopParamNameEmpty) queryParams.Add(GetSearchTopParamName(), _top.ToString());
+            if (!IsSearchSkipParamNameEmpty) queryParams.Add(GetSearchSkipParamName(), _skip.ToString());
+            if (!IsSearchTopParamNameEmpty) queryParams.Add(GetSearchTopParamName(), _top.ToString());
 
-         queryParams.Add(GetSearchCriteriaParamName(), _searchQuery.GetSearchString());
+            queryParams.Add(GetSearchCriteriaParamName(), _searchQuery.GetSearchString());
+            queryParams.Add("$mva", "true");
 
-         HttpResponseMessage response = await GetAsync(GetSearchResource(), queryParams);
+            HttpResponseMessage response = await GetAsync(GetSearchResource(), queryParams);
 
-         if (response.StatusCode != System.Net.HttpStatusCode.OK)
-         {
-            //handle according to established exception policy
-            throw (new SearchResponseException(response));
-         }
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                //handle according to established exception policy
+                throw (new SearchResponseException(response));
+            }
 
-         string responseContent = await response.Content.ReadAsStringAsync();
+            string responseContent = await response.Content.ReadAsStringAsync();
 
-         return DeserializeItems<ItemSet<T>, T>(responseContent);
-      }
+            return DeserializeItems<ItemSet<T>, T>(responseContent);
+        }
 
-      #endregion
-   }
+        #endregion
+    }
 }
