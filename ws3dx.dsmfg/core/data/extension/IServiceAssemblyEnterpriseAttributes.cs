4b33153c118ba12c85dfb77ee7ec0311aa06f55c
Antonio Nogueira <antonio.nogueira@3ds.com>
2024-09-04 12:15:51 +0200

    fix: exception calling MfgItemService.Get with IMfgItemDetailMask

--- ws3dx.dsmfg/core/data/extension/IServiceAssemblyEnterpriseAttributes.cs
+++ ws3dx.dsmfg/core/data/extension/IServiceAssemblyEnterpriseAttributes.cs
@@ -14,9 +14,12 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //------------------------------------------------------------------------------------------------------------------------------------
 using System.Collections.Generic;
+using ws3dx.dsmfg.data.impl.extension;
+using ws3dx.serialization.attribute;
 
 namespace ws3dx.dsmfg.data.extension
 {
+    [ConcreteInterfaceImpConverter(typeof(ServiceAssemblyEnterpriseAttributes))]
     public interface IServiceAssemblyEnterpriseAttributes : IDictionary<string, object>
     {
         //Specific Enterprise Attributes
