e3001edac295f32e0dc4959148071599c1702c42
Antonio Nogueira <antonio.nogueira@3ds.com>
2024-09-10 09:41:33 +0200

    Extended SecurityContext behaviour to get the role display name

--- ws3dx.core/data.impl/SecurityContext.cs
+++ ws3dx.core/data.impl/SecurityContext.cs
@@ -14,17 +14,135 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ------------------------------------------------------------------------------------------------------------------------------------
 
+using System.Data;
+
 namespace ws3dx.core.data.impl
 {
    public class SecurityContext
    {
-      public CollaborationSpace collabspace { get; set; }
-      public Organization organization { get; set; }
-      public Role role { get; set; }
+        public const string RESTRICTED = "(Restricted)";
 
-      public override string ToString()
-      {
-         return string.Format("{0}.{1}.{2}", role.name, organization.name, collabspace.name);
-      }
-   }
+        public const string ADMINISTRATOR = "Administrator";
+        public const string VPLM_ADMINISTRATOR = "VPLMAdmin";
+    
+        public const string VPLM_OWNER = "VPLMProjectAdministrator";
+        public const string VPLM_RESTRICTED_OWNER = "3DSRestrictedOwner";
+        public const string OWNER = "Owner";
+        public const string RESTRICTED_OWNER = RESTRICTED + OWNER;
+
+        public const string VPLM_LEADER = "VPLMProjectLeader";
+        public const string VPLM_RESTRICTED_LEADER = "3DSRestrictedLeader";
+        public const string LEADER = "Leader";
+        public const string RESTRICTED_LEADER = RESTRICTED + LEADER;
+
+        public const string VPLM_AUTHOR= "VPLMCreator";
+        public const string VPLM_RESTRICTED_AUTHOR = "3DSRestrictedAuthor";
+        public const string AUTHOR = "Author";
+        public const string RESTRICTED_AUTHOR = RESTRICTED + AUTHOR;
+
+        public const string VPLM_CONTRIBUTOR = "VPLMExperimenter";
+        public const string VPLM_RESTRICTED_CONTRIBUTOR = "3DSRestrictedContributor";
+        public const string CONTRIBUTOR = "Contributor";
+        public const string RESTRICTED_CONTRIBUTOR = RESTRICTED + CONTRIBUTOR;
+
+        public const string VPLM_VIEWER = "VPLMViewer";
+        public const string VPLM_RESTRICTED_VIEWER = "3DSRestrictedReader";
+        public const string VIEWER = "Viewer";
+        public const string RESTRICTED_VIEWER = RESTRICTED + VIEWER;
+
+        public const string VPLM_PUBLIC_READER = "VPLMSecuredCrossAccess"; //Ancestor before creation of restricted roles
+        public const string PUBLIC_VIEWER = "Public Viewer";
+
+        public static string getRoleDisplay(string _role)
+        { 
+            if (_role == null) return null;
+
+            string roleDisplay = _role;
+
+            switch (_role)
+            {
+                case VPLM_ADMINISTRATOR:
+                    roleDisplay = ADMINISTRATOR;
+                    break;
+
+                case VPLM_OWNER:
+                    roleDisplay = OWNER;
+                    break;
+
+                case VPLM_RESTRICTED_OWNER:
+                    roleDisplay = RESTRICTED_OWNER;
+                    break;
+
+                case VPLM_LEADER:
+                    roleDisplay = LEADER;
+                    break;
+
+                case VPLM_RESTRICTED_LEADER:
+                    roleDisplay = RESTRICTED_LEADER;
+                    break;
+
+                case VPLM_AUTHOR:
+                    roleDisplay = AUTHOR;
+                    break;
+
+                case VPLM_RESTRICTED_AUTHOR:
+                    roleDisplay = RESTRICTED_AUTHOR;
+                    break;
+
+                case VPLM_CONTRIBUTOR:
+                    roleDisplay = CONTRIBUTOR;
+                    break;
+
+                case VPLM_RESTRICTED_CONTRIBUTOR:
+                    roleDisplay = RESTRICTED_CONTRIBUTOR;
+                    break;
+
+                case VPLM_VIEWER:
+                    roleDisplay = VIEWER;
+                    break;
+
+                case VPLM_RESTRICTED_VIEWER:
+                    roleDisplay = RESTRICTED_VIEWER;
+                    break;
+
+                case VPLM_PUBLIC_READER:
+                    roleDisplay = PUBLIC_VIEWER;
+                    break;
+
+                default:
+                    roleDisplay = _role;
+                    break;
+            }
+
+            return roleDisplay;
+        }
+
+        public string Value { get { return ToString();}}
+
+        public string DisplayValue { get { return getDisplayValue(); } }
+
+        public SecurityContext() {
+        }
+
+        public SecurityContext(Role _role, Organization _organization, CollaborationSpace _collabSpace)
+        {
+            role         = _role;
+            organization = _organization;
+            collabspace  = _collabSpace;
+        }
+
+        public CollaborationSpace collabspace { get; set; }
+        public Organization organization { get; set; }
+        public Role role { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}.{1}.{2}", role.name, organization.name, collabspace.name);
+        }
+
+        public string getDisplayValue()
+        {
+            return string.Format("{0} - {1} - {2}", getRoleDisplay(role.name), organization.name, collabspace.name);
+        }
+    }
 }
